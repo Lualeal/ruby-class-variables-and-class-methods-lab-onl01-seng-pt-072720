b3db7bb0813b7d867de2aeae8a145a720d79f9ea
Luana Leal <luana.leal.eq@gmail.com>
2020-08-16 05:18:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,30 @@ class Song
   
   attr_accessor = name, artist, genre
   @@count = 0 
+  @@genres = []
+  @@artist = []
   
   def initialize
     @@count += 1 
+    @@genre 
+    @@artist
   end 
   
-  def count 
+  def self.count 
     @@count 
   end 
     
+    def self.genres
+      @@genres 
+    end 
     
+    def self.artists
+      @@artists
+    end 
+    
+    def self.genre_count
+    end 
+    
+    def self.artist.count 
+    end 
 end 
